3c957ab17989fc7a393b768218bbd23126cfcf4f
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-12-16 15:30:46 +0000

    Flashcards working, next step add options

--- src/AllCards.tsx
+++ src/AllCards.tsx
@@ -1,46 +1,88 @@
 import data from "./words.json";
 import { useState } from "react";
+import "./flashcard.css";
 
-interface data {
+interface words {
   English: string;
   Spanish: string;
 }
 
 export default function AllCards(): JSX.Element {
-  const wordData: data[] = data;
-  let index = 0;
-  const [currentShownCard, setCurrentShownCard] = useState<data>(wordData[0]);
-  const [shownLanguage, setShownLanguage] = useState<string>(
-    wordData[0].English
+  const wordData: words[] = data;
+  const [unusedCards, setUnusedCards] = useState<words[]>(wordData); // starts as all cards
+  const randomNumber = Math.floor(Math.random() * unusedCards.length);
+  const [flip, setFlip] = useState(false);
+  const [currentCard, setCurrentCard] = useState<words>(
+    unusedCards[randomNumber]
   );
-  const [unusedCards, setUnusedCards] = useState<data[]>(wordData); // starts as all cards
-  const [correctCards, setCorrectCards] = useState<data[]>([]);
-  const [wrongCards, setWrongCards] = useState<data[]>([]);
+  const [wrongCards, setWrongCards] = useState<words[]>([]);
+  const [correctCards, setCorrectCards] = useState<words[]>([]);
+
+  const wrongCount = wrongCards.length;
+  const correctCount = correctCards.length;
+  console.log(unusedCards, "unusedcards");
 
   const handleNext = () => {
-    index++;
-    setCurrentShownCard(wordData[index]);
-    setShownLanguage(currentShownCard.English);
-  }; // index isnt working
-
-  const handleFlip = () => {
-    setShownLanguage(currentShownCard.Spanish);
+    setCurrentCard(unusedCards[randomNumber]);
   };
 
   const handleWrong = () => {
-    setWrongCards([...wrongCards, currentShownCard]);
+    setUnusedCards([...unusedCards.filter((card) => card !== currentCard)]);
+    if (
+      !wrongCards.includes(currentCard) &&
+      !correctCards.includes(currentCard)
+    ) {
+      setWrongCards([...wrongCards, currentCard]);
+    }
   };
   const handleCorrect = () => {
-    setCorrectCards([...correctCards, currentShownCard]);
+    setUnusedCards([...unusedCards.filter((card) => card !== currentCard)]);
+    if (
+      !correctCards.includes(currentCard) &&
+      !wrongCards.includes(currentCard)
+    ) {
+      setCorrectCards([...correctCards, currentCard]);
+    }
   };
 
   return (
-    <div>
-      <p> {shownLanguage}</p>
-      <button onClick={handleFlip}> Flip Card </button>
-      <button onClick={handleWrong}> Wrong</button>
-      <button onClick={handleCorrect}> Correct </button>
-      <button onClick={handleNext}> Next card </button>
+    <div className="page">
+      <h1> Spanish/English flashcard game</h1>
+      <div className="card">
+        <p className="text">
+          {flip ? (
+            <img className="flags" src="./utils/englishflag.png" alt="" />
+          ) : (
+            <img className="flags" src="./utils/spanishflag.png" alt="" />
+          )}
+          <br />
+          {flip ? currentCard.English : currentCard.Spanish}
+        </p>
+      </div>
+      <button className="button-wrong" onClick={handleWrong}>
+        {" "}
+        Incorrect{" "}
+      </button>
+      <button className="button-correct" onClick={handleCorrect}>
+        {" "}
+        Correct{" "}
+      </button>
+      <br />
+      <br />
+      <button className="button-24" onClick={() => setFlip(!flip)}>
+        {" "}
+        Flip Card{" "}
+      </button>
+      <button className="button-24" onClick={handleNext}>
+        {" "}
+        Next card{" "}
+      </button>
+      <br />
+      <p>
+        {" "}
+        Words to revise = {wrongCount} <br /> Correct words = {correctCount}{" "}
+        <br /> Words left to test: {unusedCards.length}
+      </p>
     </div>
   );
 }
