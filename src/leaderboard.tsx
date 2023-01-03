305c674628e76a2b0dd4c0bb5889c59707113fb5
Katrina <katrinalea.woolley@yahoo.co.uk>
2023-01-03 12:01:38 +0000

    connected to elephant sql database, currently running on local host only

--- src/leaderboard.tsx
+++ src/leaderboard.tsx
@@ -1,3 +1,47 @@
+import {useEffect, useState} from "react"
+import axios from "axios";
+
+interface LBoard {
+  id: number,
+  name: string,
+  correct: number,
+  testAmount: number
+}
+
 export default function Leaderboard(): JSX.Element {
-  return <p> Leaderboard </p>;
+  const [leaderboard, setLeaderboard] = useState<LBoard[]>([])
+
+     useEffect(() => {
+    const fetchAPI = async () => {
+      const response = await axios.get(
+      "http://localhost:4000/names"
+      );
+      const fetchedWholeObject = response.data;
+      const fetchedTasks = fetchedWholeObject.data;
+      // sets tasks to the data
+      setLeaderboard(fetchedTasks);
+    };
+    fetchAPI();
+  }, [leaderboard]);
+
+  return (
+    <div>
+    <h1> Leaderboard </h1>
+    <table> 
+      <tr>
+      <th>Name</th>
+      <th>Score</th>
+      </tr>
+      {leaderboard.map(person => 
+      <>
+      <tr>
+      <td>{person.name}</td>
+      <td>{person.correct}/{person.testAmount}</td>
+      </tr>
+      </>
+      )
+}
+    </table>
+    </div>
+  );
 }
